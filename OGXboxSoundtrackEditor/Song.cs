3b7d729b034f23666fb0dc2338e915f5b235f952
ChaoticWolf <65677836+ChaoticWolf@users.noreply.github.com>
2025-06-12 13:40:53 -0400

    Remove usings

--- OGXboxSoundtrackEditor/Song.cs
+++ OGXboxSoundtrackEditor/Song.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OGXboxSoundtrackEditor
+﻿namespace OGXboxSoundtrackEditor
 {
     public class Song
     {
